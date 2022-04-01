59b85f84cb847a618ca5bd2c54280e5034959080
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-01 10:52:28 +0530

    feat: added quote to the db

---
+++ src/Data.jsx
@@ -0,0 +1,24 @@
+const QuoteData =[
+    {
+        "quote":" “The purpose of our lives is to be happy.” "
+    },
+    {
+        "quote":"Never let the fear of striking out keep you from playing the game."
+    },
+    {
+        "quote":"Turn your wounds into wisdom."
+    },
+    {
+        "quote":"I like criticism. It makes you strong.”"
+    },
+    {
+        "quote":"If you choose, you can be joyful every moment of your life. It’s time you made your choice."
+    },
+    {
+        "quote":"Once your life is an expression of your blissfulness, you will not be in conflict with anyone."
+    },
+    {
+        "quote":"Life! Everyone has a lifetime's experience with it, but only a few manage to make the most of it. What's the secret to living life to the fullest? Here are 5 quotes from Sadhguru."
+    },
+]
+   
