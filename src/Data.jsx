f6b0bfd0efe71d627e80623f60b914c082fa1992
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-02 14:24:18 +0530

    style: bg image added

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,7 +1,7 @@
 const QuoteData =[
     {
         "quote":" The purpose of our lives is to be happy. ",
-        "img":"https://images.unsplash.com/photo-1635239239608-40c8dd5b895e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        "img":"https://images.unsplash.com/photo-1552872673-9b7b99711ebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8MTkyMCUyMHglMjAxMDgwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
         "quote":"Never let the fear of striking out keep you from playing the game.",
@@ -12,16 +12,21 @@ const QuoteData =[
         "img":"https://images.unsplash.com/photo-1635239239608-40c8dd5b895e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
     },
     {
-        "quote":"I like criticism. It makes you strong.”"
+        "quote":"I like criticism. It makes you strong.",
+        "img":"https://wallpapercave.com/dwp1x/c5agh56.jpg"
     },
     {
-        "quote":"If you choose, you can be joyful every moment of your life. It’s time you made your choice."
+        "quote":"If you choose, you can be joyful every moment of your life. It’s time you made your choice.",
+        "img":"https://wallpapercave.com/dwp1x/OmrKqpj.jpg"
     },
     {
-        "quote":"Once your life is an expression of your blissfulness, you will not be in conflict with anyone."
+        "quote":"Once your life is an expression of your blissfulness, you will not be in conflict with anyone.",
+
+        "img":"https://wallpapercave.com/dwp1x/DQfivR7.jpg"
     },
     {
-        "quote":"Life! Everyone has a lifetime's experience with it, but only a few manage to make the most of it. What's the secret to living life to the fullest? Here are 5 quotes from Sadhguru."
+        "quote":"Life! Everyone has a lifetime's experience with it, but only a few manage to make the most of it. What's the secret to living life to the fullest? Here are 5 quotes from Sadhguru.",
+        "img":"https://wallpapercave.com/dwp1x/EKQ7Pw6.jpg"
     },
 ]
    
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,50 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import {QuoteData} from "../Data";
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { QuoteData } from "../Data";
+import axios from "axios";
+import "../Home/Home.css";
 
 const Home = () => {
-    const randomQuote= Math.floor(Math.random() * Math.floor(QuoteData.length))
-    console.log(randomQuote);
+  const randomQuote = Math.floor(Math.random() * Math.floor(QuoteData.length));
+  console.log(randomQuote);
 
-    const [data,setData]= useState({})
+  const newDate = Date();
+  console.log(newDate);
 
-    const url = "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&city=Nagpur&country=India&key=cffbc1552ecc4605bb2e50e9a86243ab"
+  const [data, setData] = useState({});
 
-    useEffect(()=>{
-        axios.get(url)
-        .then((res)=> setData(res.data.data[0]))
-    },[])
+  const url =
+    "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&city=Nagpur&country=India&key=cffbc1552ecc4605bb2e50e9a86243ab";
 
-  const apiData= [{cityName:data.city_name,date:data.datetime, temp:data.temp,
-                  windSpeed:data.wind_spd, windDirection:data.wind_cdir_full
-                 }]
+  useEffect(() => {
+    axios.get(url).then((res) => setData(res.data.data[0]));
+  }, []);
 
-    console.log("data from state",data);
-    console.log("api data",apiData)
+  const apiData = [
+    {
+      cityName: data.city_name,
+      date: data.datetime,
+      temp: data.temp,
+      windSpeed: data.wind_spd,
+      windDirection: data.wind_cdir_full,
+    },
+  ];
 
   return (
-    <div>
-    <h1>infinite-web-extension</h1>
-    <h1>{QuoteData[randomQuote].quote}</h1>
-    <div>
-   <img src={QuoteData[randomQuote].img}>
-   
-   </img> 
-   <h1>{apiData.map(({cityName,date,temp,windSpeed,windDirection}) =>{
-     return(
-       <div>
-       <h1>{cityName}</h1>
-       <p>{date}</p>
-       <p>{temp}</p>
-       <p>{windDirection}</p>
-       <p>{windSpeed}m/s</p>
-       
-       </div>
-     )
-   })}</h1>
+    <div className="bg-img-container">
+      <h1>
+        {apiData.map(({ cityName, date, temp, windSpeed, windDirection }) => {
+          return (
+            <div
+              className="weather"
+              style={{
+                backgroundImage: `url(${QuoteData[randomQuote].img})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                height: "100%",
+              }}
+            >
+              <div className="display-row topnav text-color">
+                <p>{cityName}</p>
+                <p>
+                  <span>
+                    <i class="bi bi-clouds"></i>
+                  </span>
+                  {temp}°C
+                </p>
+              </div>
+              <div className="quote text-color">
+                <h5>{QuoteData[randomQuote].quote}</h5>
+                <p>{date}</p>
+              </div>
+              <div className="display-row footer text-color">
+                <p>{windDirection}</p>
+                <p>WindSpeed : {windSpeed}(m/s)</p>
+              </div>
+            </div>
+          );
+        })}
+      </h1>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export {Home}
+export { Home };
